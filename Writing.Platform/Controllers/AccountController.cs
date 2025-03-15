846d9cad02b0e9b0f8c75f6388cdb5a19f731cc3
monikhoder <rinmorni@gmail.com>
2025-03-15 12:09:22 +0700

    Add User register

---
+++ Writing.Platform/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Writing.Platform.Data;
+using Writing.Platform.Models.ViewModel;
+
+namespace Writing.Platform.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterAccountRequest registerRequest)
+        {
+            var newUser = new IdentityUser
+            {
+                UserName = registerRequest.userName,
+                Email = registerRequest.Email,
+            };
+            var register = userManager.CreateAsync(newUser, registerRequest.Password);
+            if (register.Result.Succeeded)
+            {
+                var addRole = userManager.AddToRoleAsync(newUser, "User");
+                if (addRole.Result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ Writing.Platform/Models/ViewModel/RegisterAccountRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Writing.Platform.Models.ViewModel
+{
+    public class RegisterAccountRequest
+    {
+        public string userName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
